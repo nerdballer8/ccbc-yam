c062de386b9232d3751c68f58754767f7dcfe76d
mauiworketc <maui@worketc.com>
2019-08-20 21:23:55 +0800

    latest

--- src/app/staff/staff.page.ts
+++ src/app/staff/staff.page.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-staff',
   templateUrl: './staff.page.html',
-  styleUrls: ['./staff.page.scss'],
+  styleUrls: ['./staff.page.scss']
 })
 export class StaffPage implements OnInit {
+  Staff$: Observable<any>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public api: ApiService) {
+    this.Staff$ = this.api.getValues('STAFF!B2:E53', 'ROWS');
   }
 
+  ngOnInit() {}
 }
